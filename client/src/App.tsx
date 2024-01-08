1ea33958c9cda3e6b8c961b2ae861a5f1f9431fd
Michael Berezovsky <mishka.ber@gmail.com>
2024-01-08 18:43:32 +0200

    Add game page component to app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import GamePage from "./components/game-page";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Tic-Tac-Toe</h1>
+        <GamePage />
       </header>
     </div>
   );
